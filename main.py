1c4a0717dd8640fb76539b106c74d995358fd09f
Melvillian <melvillevt@gmail.com>
2023-12-23 12:58:13 -0500

    add Black formatter, flake8 linter, and mypy using vscode.

--- main.py
+++ main.py
@@ -14,29 +14,32 @@ NOTION_API_PREFIX = "https://api.notion.com/v1"
 headers = {
     "Authorization": f"Bearer {NOTION_KEY}",
     "Content-Type": "application/json",
-    "Notion-Version": NOTION_VERSION
+    "Notion-Version": NOTION_VERSION,
 }
 # TODO: handle non-200 responses everywhere
 
-def fetch_page(page_id):  
+
+def fetch_page(page_id):
     url = f"{NOTION_API_PREFIX}/pages/{page_id}"
     response = requests.get(url, headers=headers)
     page_data = response.json()
     return page_data
 
+
 def search_for_pages(search_query=None):
     """
-    Searches for pages in the user's Notion workspace based on which pages have been shared with the integration
+    Searches for pages in the user's Notion workspace
 
-    Based on the Notion API key you're using, return all of the pages' properties (but not their content)
-    in the user's workspace. This contains page ID's which can be used to access each page's
-    content using the block API
+    Based on the Notion API key you're using and the pages that have been
+    shared with the key's integration, return all of the pages' properties
+    (but not their content) in the user's workspace. This contains page ID's
+    which can be used to access each page's content using the block API
 
     Args:
-        search_query (str): optional page name to search for in the Notion workspace (Example: "liberalism")
+        search_query (str): page name to search for, for search all if None
 
     Returns:
-        dict: a dictionary of search result data, which includes cursor fields as well as a `results` list
+        dict: a dictionary of search results and cursor data
         {
             "object": "list",
             "results": [
@@ -98,37 +101,38 @@ def search_for_pages(search_query=None):
 
     # TODO: handle pagination
 
-    search_params = {"filter": {
-            "value": "page",
-            "property": "object"
-        },
-        "sort": {
-            "direction": "ascending",
-            "timestamp": "last_edited_time"
-        }
+    search_params = {
+        "filter": {"value": "page", "property": "object"},
+        "sort": {"direction": "ascending", "timestamp": "last_edited_time"},
     }
 
     if search_query:
-        # if you don't provide a query, then it will search all pages connected to the Notion integration
+        # if you don't provide a query, then it will search all pages
+        # connected to the Notion integration
         search_params["query"] = search_query
 
     search_response = requests.post(
-        f'{NOTION_API_PREFIX}/search',
-        json=search_params, headers=headers)
+        f"{NOTION_API_PREFIX}/search", json=search_params, headers=headers
+    )
 
     return search_response.json()
+
 
 def create_mention_section(mention_page_name):
     """
     Create a mention section for the paragraph block.
 
-    This is the real purpose of the script, to create these mention sections for a paragraph
+    This is the real purpose of the script, to create these mention sections
+    for a paragraph
     """
 
     response = search_for_pages(mention_page_name)
 
     results = response["results"]
-    assert len(results) == 1, f"There should only be one page with this name {mention_page_name}, but instead we found the results: {json.dumps(results, indent=4, sort_keys=True)}"
+    assert len(results) == 1, (
+        f"There should only be one page with this name {mention_page_name},"
+        f"but instead we found the results: {json.dumps(results, indent=4, sort_keys=True)}"
+    )
     page_id = results[0]["id"]
     href = results[0]["url"]
 
@@ -139,26 +143,23 @@ def create_mention_section(mention_page_name):
             "color": "default",
             "italic": False,
             "strikethrough": False,
-            "underline": False
+            "underline": False,
         },
         "href": href,
-        "mention": {
-            "page": {
-                "id": page_id
-            },
-            "type": "page"
-        },
+        "mention": {"page": {"id": page_id}, "type": "page"},
         "plain_text": mention_page_name,
-        "type": "mention"
+        "type": "mention",
     }
 
     return new_section
+
 
 def create_text_section(section_text):
     """
     Create a text section for the paragraph block.
 
-    This is pretty boring, because it just contains simple plaintext, no mentions
+    This is pretty boring, because it just contains simple plaintext,
+    no mentions
     """
     new_section = {
         "annotations": {
@@ -167,18 +168,16 @@ def create_text_section(section_text):
             "color": "default",
             "italic": False,
             "strikethrough": False,
-            "underline": False
+            "underline": False,
         },
         "href": None,
         "plain_text": section_text,
-        "text": {
-            "content": section_text,
-            "link": None
-        },
-        "type": "text"
+        "text": {"content": section_text, "link": None},
+        "type": "text",
     }
 
     return new_section
+
 
 def update_a_block(block_id, block):
     """
@@ -192,72 +191,98 @@ def update_a_block(block_id, block):
             "text": None,
         }
     }
-    needs_update = False # update this to True if this block contains any literals [[...]] we need to turn into mentions
+    # update this to True if this block contains any
+    # literals [[...]] we need to turn into mentions
+    needs_update = False
     if block["type"] == "paragraph":
         old_paragraph = block["paragraph"]
         if not old_paragraph["text"]:
-            # this is a boring empty paragraph, so we do not update anything and simply return
+            # this is a boring empty paragraph, so we do not update
+            # anything and simply return
             return
-    
-        # start building the new paragraph that we'll use to update (i.e. overwrite) the old paragraph block
+
+        # start building the new paragraph that we'll use to update
+        # (i.e. overwrite) the old paragraph block
         new_paragraph_block["paragraph"]["color"] = old_paragraph["color"]
         new_paragraph = []
         for paragraph_section in old_paragraph["text"]:
             virtual_text = create_virtual_text(paragraph_section["plain_text"])
 
             if not any(tup[1] for tup in virtual_text):
-                # this section of paragraph doesn't contain any literal [[...]] text which should be turned
-                # into mentions, so we should leave it as is by simply appending the existing old section to
-                # the new paragraph's content
+                # this section of paragraph doesn't contain any literal [[...]]
+                # text which should be turned into mentions, so we should leave
+                # it as is by simply appending the existing old section to the
+                # new paragraph's content
                 new_paragraph.append(paragraph_section)
                 continue
-    
+
             needs_update = True
-            # this section of paragraph contains literal [[...]] text which should be turned into mentions
-            # so we'll need to build a new section for each mention and for each plaintext, and append it to the new paragraph
+            # this section of paragraph contains literal [[...]] text
+            # which should be turned into mentions so we'll need to
+            # build a new section for each mention and for each plaintext,
+            # and append it to the new paragraph
             for section in virtual_text:
                 section_text = section[0]
                 is_mention = section[1]
-                new_section = create_mention_section(section_text) if is_mention else create_text_section(section_text) 
+                new_section = (
+                    create_mention_section(section_text)
+                    if is_mention
+                    else create_text_section(section_text)
+                )
                 new_paragraph.append(new_section)
 
         new_paragraph_block["paragraph"]["text"] = new_paragraph
     else:
-        print(f"Found a non-paragraph block of type {block['type']} in block {block_id}.")
+        print(
+            f"Found a non-paragraph block of type {block['type']} in block {block_id}."
+        )
         print("BLOCK:")
         print(block)
         print("Aborting update")
         sys.exit(0)
 
     if not needs_update:
-        print("No literal [[...]] sections found in this block, so we'll not update it.")
+        print(
+            (
+                "No literal [[...]] sections found in this block,"
+                " so we'll not update it."
+            )
+        )
         return None
 
     print(json.dumps(old_paragraph, indent=4, sort_keys=True))
 
-    url = f'{NOTION_API_PREFIX}/blocks/{block_id}'
+    url = f"{NOTION_API_PREFIX}/blocks/{block_id}"
 
-    proceed = input(f"{json.dumps(new_paragraph_block, indent=4, sort_keys=True)}\ntype 'y' if you wish to proceed with the above patch update to block id: {block_id}... (y/n)")
+    proceed = input(
+        (
+            f"{json.dumps(new_paragraph_block, indent=4, sort_keys=True)}\n"
+            "type 'y' if you wish to proceed with the above patch update to"
+            f"block id: {block_id}... (y/n)"
+        )
+    )
 
     if proceed == "y":
-        print('you proceeded')
+        print("you proceeded")
         response = requests.patch(url, headers=headers, json=new_paragraph_block)
         return response.json()
-    
+
     return None
+
 
 def fetch_block_children(block_id, page_name):
     """
-    Given a Block ID (which might also be a Page ID), return a dict keyed by all of the given
-    block's childrens' IDs, and the child's data, THOUGH IT WILL ONLY RETURN CHILDREN THAT ARE
-    PARAGRAPHS.
-    
-    The important value will be the `paragraph` field, which contains an array of
-    objects of type `text` and `mention` (there could also be equation in the original block, but
-    we ignore those)
+    Given a Block ID (which might also be a Page ID), return a dict keyed by
+    all of the given block's childrens' IDs, and the child's data, THOUGH IT
+    WILL ONLY RETURN CHILDREN THAT ARE PARAGRAPHS.
+
+    The important value will be the `paragraph` field, which contains an
+    array of objects of type `text` and `mention` (there could also be equation
+    in the original block, but we ignore those)
 
     Returns:
-        dict: a dict keyed by block ID, and the value is a dict containing the block's type. For example:
+        dict: a dict keyed by block ID, and the value is a dict containing the
+        block's type. For example:
         {
             "blocks": {
                 "13b5fa46-4308-4e19-a22b-67d440a017b6": {
@@ -424,8 +449,9 @@ def fetch_block_children(block_id, page_name):
             "next_cursor": null
         }
     """
-    # (TODO: need to handle pagination via `page_size`: see https://developers.notion.com/reference/intro#pagination)
-    url = f'{NOTION_API_PREFIX}/blocks/{block_id}/children'
+    # (TODO: need to handle pagination via `page_size`: see
+    # https://developers.notion.com/reference/intro#pagination)
+    url = f"{NOTION_API_PREFIX}/blocks/{block_id}/children"
     response = requests.get(url, headers=headers)
     response = response.json()
 
@@ -436,7 +462,8 @@ def fetch_block_children(block_id, page_name):
     }
 
     for block in response["results"]:
-        # (TODO: handle non-paragraph types, like: bulleted-list, headings (for their blockren), numbered list item)
+        # (TODO: handle non-paragraph types, like: bulleted-list,
+        # headings (for their blockren), numbered list item)
         if block["type"] == "paragraph":
             block_children["blocks"][block["id"]] = {
                 "has_children": block["has_children"],
@@ -447,7 +474,7 @@ def fetch_block_children(block_id, page_name):
             print(f'Skipping non-paragraph block: {block["id"]}')
 
     if len(block_children["blocks"]) == 0:
-        print(f'No children found for block: ID: {block_id} Page Name: {page_name}')
+        print(f"No children found for block: ID: {block_id} Page Name: {page_name}")
 
     return block_children
 
@@ -458,17 +485,23 @@ if __name__ == "__main__":
     output = search_for_pages()
 
     for page in output["results"]:
-
         # get an arbitrary page and print out useful info
         title_data = page["properties"]["title"]["title"]
-        assert len(title_data) == 1, f"only one title allowed per page, but found {len(title_data)} for page:\n{page_name}"
+        assert len(title_data) == 1, (
+            f"only one title allowed per page, but found {len(title_data)}"
+            f"for page:\n{title_data[0]['plain_text']}"
+        )
         page_name = title_data[0]["plain_text"]
-        assert page_name == title_data[0]["text"]["content"], f"title data is not consistent: {page_name}, {title_data[0]['text']['content']}"
+        assert page_name == title_data[0]["text"]["content"], (
+            f"title data is not consistent: "
+            f"{page_name}, {title_data[0]['text']['content']}"
+        )
         page_id = page["id"]
         print(f"Page Name: {page_name}")
         print(f"Page ID: {page_id}")
 
-        # process the first-layer children on that page (TODO: recurse through block sub-children to get all data)
+        # process the first-layer children on that page
+        # (TODO: recurse through block sub-children to get all data)
         block_children = fetch_block_children(page_id, page_name)
         for block_id, block in block_children["blocks"].items():
             response = update_a_block(block_id, block)
